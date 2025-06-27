10a994e3d96429c95a904d1dc19515270e133e4c
ciarangrant <31352641+ciarangrant@users.noreply.github.com>
2025-06-27 11:04:31 +0100

    Create set_release_update_type.sh

---
+++ .github/scripts/set_release_update_type.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# set_release_update_type.sh
+# Checks the format of the release notes texts and extracts the update type from it
+#
+# Usage: set_release_update_type.sh
+#
+# Outputs the extracted content to stdout.
+# Exits with 0 if a valid update type is found, otherwise exits with 1
+
+# Parameters
+RELEASE_NOTES=$(cat -)
+echo "$RELEASE_NOTES"
+
+UNRELEASED_CHANGES=$(grep "^## \[Unreleased\]" $RELEASE_NOTES)
+
+if [ -z "$UNRELEASED_CHANGES" ] # If UNRELEASED_CHANGES is empty
+then
+  echo "$RELEASE_NOTES doesn't contain unreleased changes"
+  exit 1
+else
+  echo "$RELEASE_NOTES contains unreleased changes: $UNRELEASED_CHANGES"
+fi
+
+UPDATE_TYPE_REGEX='^## \[Unreleased\] - (MAJOR|MINOR|PATCH)' # Capture MAJOR|MINOR|PATCH in group 1
+if [[ $UNRELEASED_CHANGES =~ $UPDATE_TYPE_REGEX ]]
+then
+  UPDATE_TYPE="${BASH_REMATCH[1]}" # Set UPDATE_TYPE to group 1 content
+  echo "$RELEASE_NOTES contains update type ${UPDATE_TYPE}"
+else
+  echo "$RELEASE_NOTES doesn't contain update type"
+  exit 1
+fi
+"UPDATE_TYPE=$UPDATE_TYPE"
+echo "$UPDATE_TYPE" 
+exit 0
+        
