fabbf063a16d5c7cf76fcc496e508714271fcd73
ciarangrant <31352641+ciarangrant@users.noreply.github.com>
2025-06-27 11:41:03 +0100

    Update set_release_update_type.sh

--- .github/scripts/set_release_update_type.sh
+++ .github/scripts/set_release_update_type.sh
@@ -14,19 +14,19 @@ UNRELEASED_CHANGES=$(echo "$RELEASE_NOTES" | grep "^## \[Unreleased\]")
 
 if [ -z "$UNRELEASED_CHANGES" ] # If UNRELEASED_CHANGES is empty
 then
-  echo "$RELEASE_NOTES doesn't contain unreleased changes"
+  echo "$RELEASE_NOTES doesn't contain unreleased changes" >&2
   exit 1
 else
-  echo "$RELEASE_NOTES contains unreleased changes: $UNRELEASED_CHANGES"
+  echo "$RELEASE_NOTES contains unreleased changes: $UNRELEASED_CHANGES" >&2
 fi
 
 UPDATE_TYPE_REGEX='^## \[Unreleased\] - (MAJOR|MINOR|PATCH)' # Capture MAJOR|MINOR|PATCH in group 1
 if [[ $UNRELEASED_CHANGES =~ $UPDATE_TYPE_REGEX ]]
 then
   UPDATE_TYPE="${BASH_REMATCH[1]}" # Set UPDATE_TYPE to group 1 content
-  echo "$RELEASE_NOTES contains update type ${UPDATE_TYPE}"
+  echo "$RELEASE_NOTES contains update type ${UPDATE_TYPE}" >&2
 else
-  echo "$RELEASE_NOTES doesn't contain update type"
+  echo "$RELEASE_NOTES doesn't contain update type" >&2
   exit 1
 fi
 #"UPDATE_TYPE=$UPDATE_TYPE"
